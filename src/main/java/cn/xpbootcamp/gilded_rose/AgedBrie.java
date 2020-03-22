71e0c01f07b2569cae16cd3acc957be705f6e4e9
hfguan <hfguan@thoughtworks.com>
2020-03-22 17:33:26 +0800

    add model

---
+++ src/main/java/cn/xpbootcamp/gilded_rose/AgedBrie.java
@@ -0,0 +1,7 @@
+package cn.xpbootcamp.gilded_rose;
+
+import java.time.LocalDate;
+
+public class AgedBrie extends Goods{
+    private LocalDate sellIn;
+}
---
+++ src/main/java/cn/xpbootcamp/gilded_rose/BackstagePass.java
@@ -0,0 +1,7 @@
+package cn.xpbootcamp.gilded_rose;
+
+import java.time.LocalDate;
+
+public class BackstagePass extends Goods {
+    private LocalDate sellIn;
+}
